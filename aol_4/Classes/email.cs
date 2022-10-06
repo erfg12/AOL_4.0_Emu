22a9247bd7e103b6baa575556b0bc49af979bfa8
Jacob Fliss <newgmobile@gmail.com>
2022-10-06 16:03:40 -0400

    Update email.cs

--- aol_4/Classes/email.cs
+++ aol_4/Classes/email.cs
@@ -26,7 +26,7 @@ namespace aol.Classes
         public static ConcurrentDictionary<string, string> emailsSent = new ConcurrentDictionary<string, string>();
         public static string reply = "";
         public static bool youGotMail = false;
-        private static string host = "mail.newagesoftware.net";
+        private static string host = "ns65v.stableserver.net";
         private static int imapPort = 993;
         private static int smtpPort = 465;
 
