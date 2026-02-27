60197ce9360172c2d3c6807ed7765c1fdab0630d
Jacob Fliss <newgmobile@gmail.com>
2026-02-26 19:01:10 -0500

    Update VersionHelper.cs

--- aol_4/Helpers/VersionHelper.cs
+++ aol_4/Helpers/VersionHelper.cs
@@ -17,6 +17,7 @@ public class VersionHelper
         try
         {
             using var client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("AOL-4.0-Emu");
             var response = client.GetStringAsync("https://api.github.com/repos/erfg12/AOL_4.0_Emu/releases/latest").Result;
             return JsonDocument.Parse(response).RootElement.GetProperty("tag_name").GetString() ?? "1.0.999.0";
         }
